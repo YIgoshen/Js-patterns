f2ed16e07f0034224d94a60c6eeb6f4ec99637f9
YIgoshen <aigul@powr.io>
2021-06-23 23:37:28 +0600

    create constructor pattern

---
+++ 1_creational/1_constructor.js
@@ -0,0 +1,27 @@
+// Наиболее часто встречаемый паттерн в js, который позволяет создавать нам новые объекты определенного типа
+
+ 
+//before creation of the Class
+// function Server(name, ip) {
+//    this.name = name
+//    this.ip = ip
+//  }
+
+//  Server.prototype.getUrl = function() {
+//    return `https://${this.ip}:80`
+//  }
+
+
+class Server {
+  constructor(name, ip) {
+    this.name = name
+    this.ip = ip
+  }
+  getUrl() {
+    return `https://${this.ip}:80`
+  }
+}
+
+
+const aws = new Server('AWS German', '82.31.21.32')
+console.log(aws.getUrl());
